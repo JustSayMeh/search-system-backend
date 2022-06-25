20e459b9d7b10fe707dbd4dac1956452dbdf4e88
Олег Завьялов <O.romanowi4@gmail.com>
2022-06-25 16:28:51 +0700

    init commit

---
+++ src/test/java/com/example/searchsystembackend/SearchSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchsystembackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
