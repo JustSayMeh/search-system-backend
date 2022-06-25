2397142a3fb2580e04d1b0accab45be155e19c86
Олег Завьялов <O.romanowi4@gmail.com>
2022-06-26 02:05:06 +0700

    фикс получения ролей

--- src/main/java/ru/scytech/documentsearchsystembackend/security/CsrfTokenController.java
+++ src/main/java/ru/scytech/documentsearchsystembackend/security/CsrfTokenController.java
@@ -2,13 +2,18 @@ package ru.scytech.documentsearchsystembackend.security;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = "/csrfToken")
@@ -21,8 +26,8 @@ public class CsrfTokenController {
     }
 
     @GetMapping("/role")
-    public List<String> getUserRole(SecurityProperties.User user) {
-        return user.getRoles();
-
+    public List<String> getUserRole() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream().map(it -> it.getAuthority()).collect(Collectors.toList());
     }
 }
